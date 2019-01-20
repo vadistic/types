176f3a63045f0e716868eee654df8c41d020b76a
Jakub Wadas <vadistic@gmail.com>
2019-01-20 13:03:03 +0100

    util: readonly & nonundefined

--- packages/types-util/index.d.ts
+++ packages/types-util/index.d.ts
@@ -1,1 +1,11 @@
-export type SimpleLiteral = string | number
+export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+
+export type Readonly<T extends object> = { readonly [K in keyof T]: T[K] }
+export type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K]
+}
+
+export type NonUndefined<T> = T extends undefined ? never : T
+export type NonUndefinedProps<T extends object> = {
+  [K in keyof T]?: NonUndefined<T[K]>
+}
