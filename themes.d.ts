b0b7d68f73ae53ddb3f50690a9a3f240d3212a6d
Jakub Wadas <vadistic@gmail.com>
2019-01-20 12:25:28 +0100

    gitpkg

---
+++ themes.d.ts
@@ -0,0 +1,19 @@
+// TypeScript Version: 3.0
+
+declare module 'grommet-icons/themes' {
+  export const base: {
+    global: {
+      colors: {
+        icon: string
+      }
+    }
+    icon: {
+      size: {
+        small: string
+        medium: string
+        large: string
+        xlarge: string
+      }
+    }
+  }
+}
