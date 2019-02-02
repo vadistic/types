77fd76a364482a7c938522a28e08c35eb462e1ad
Jakub Wadas <vadistic@gmail.com>
2019-02-02 10:08:27 +0100

    update gatsby types

--- packages/gatsby/index.d.ts
+++ packages/gatsby/index.d.ts
@@ -1,27 +1,25 @@
+import * as React from 'react'
+
+export {
+  default as Link,
+  GatsbyLinkProps,
+  navigate,
+  navigateTo,
+  push,
+  replace,
+  withPrefix,
+} from 'gatsby-link'
+
 declare module 'gatsby' {
-  import * as React from 'react'
-
-  export {
-    default as Link,
-    GatsbyLinkProps,
-    navigate,
-    navigateTo,
-    push,
-    replace,
-    withPrefix,
-  } from 'gatsby-link'
-
   type RenderCallback<Data> = (data: Data) => React.ReactNode
 
-  export interface StaticQueryProps<Data> {
+  interface StaticQueryProps<Data> {
     query: any
     render?: RenderCallback<Data>
     children?: RenderCallback<Data>
   }
 
-  export class StaticQuery<Data> extends React.Component<
-    StaticQueryProps<Data>
-  > {}
+  class StaticQuery<Data> extends React.Component<StaticQueryProps<Data>> {}
 
-  export function graphql(query: TemplateStringsArray): void
+  const graphql: (query: TemplateStringsArray) => void
 }
